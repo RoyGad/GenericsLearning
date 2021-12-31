acdd10df697392d97773c199564a1e28cb5bb63a
RoyGad <37749388+RoyGad@users.noreply.github.com>
2021-12-31 11:09:15 +0000

    Add files via upload
    Starting repo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using GenericsLearning.Services;
+using GenericsLearning.Models;
+
+namespace GenericsLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Current time is: " + DateTime.Now);
+
+            var studentService = new StudentPrinterService(new StudentRepository());
+            studentService.PrintStudents();
+
+            Console.WriteLine("________________________________________________");
+
+            var authorService = new AuthorPrinterService(new AuthorRespository());
+            authorService.PrintAuthors();
+        
+        }
+    }
+}
+
